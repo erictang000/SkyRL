129a58f389c185425f350a31d62b756030f27070
Eric Tang <erictang000@gmail.com>
2025-09-26 21:37:05 +0000

    x

--- skyrl-train/skyrl_train/distributed/megatron/optimizer.py
+++ skyrl-train/skyrl_train/distributed/megatron/optimizer.py
@@ -24,7 +24,6 @@ from megatron.core.optimizer_param_scheduler import OptimizerParamScheduler
 
 
 def init_megatron_optim_config(optim_config: dict, optimizer_config_kwargs: dict) -> OptimizerConfig:
-    # TODO (erictang000): add support for pass through megatron optimizer kwargs
     optim_args = {
         "optimizer": optim_config.get("optimizer", "adam"),
         "lr": optim_config.get("lr"),
