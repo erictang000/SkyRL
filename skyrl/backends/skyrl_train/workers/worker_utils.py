[fix] Include masked out batches in metrics calculation with `max_token_per_microbatch > 0` (#1817)
# What does this PR do?


After #1477 , we skip all micro batches where the `loss_mask` is zero
while calculating batch metrics during `forward_backward`. The original
rationale is that padding batches have loss masks that are all zeros.
However, this also skips masked out trajectories from the metrics
calculation. This is a regression from the previous metrics calculation
behaviour.

Further, this can also lead to subtle hangs: if a mini batch shard
received by a DP rank has only masked out trajectories, then the
`all_metrics` dict is empty because `loss_mask` is zero for all micro
batches. This can lead to a NCCL timeout where the DP rank will exit
metrics calculation early on :
https://github.com/NovaSky-AI/SkyRL/blob/7495d61f85a476b77b666525d80d9f728dc961b5/skyrl/backends/skyrl_train/workers/megatron/megatron_worker.py#L1120-L1122
(dict is empty)
 
 while other ranks wait for this DP rank to join in an all reduce.

---------

Signed-off-by: SumanthRH <sumanthrh@anyscale.com>

skyrl/backends/skyrl_train/workers/worker_utils.py
@@ -273,7 +273,10 @@ class TokenBasedBatchIterator(BaseBatchIterator):
             data["rollout_expert_indices"] = torch.zeros(
                 (batch_size, *ref_tensor.shape[1:]), dtype=ref_tensor.dtype, device=device
             )
-        data.metadata = self.data.metadata
+        data.metadata = {}
+        if self.data.metadata:
+            data.metadata.update(self.data.metadata)
+        data.metadata["is_padding_batch"] = True
         return data
 
     def _sync_num_microbatches(self) -> int: